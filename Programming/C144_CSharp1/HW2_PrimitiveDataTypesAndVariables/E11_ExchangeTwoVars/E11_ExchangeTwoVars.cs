cb9c8d33d0afb3f8ccf133dbfb08a0b6797d2093
skaniol <skaniol@abv.bg>
2013-11-14 20:13:21 +0200

    Exercise 11

---
+++ Programming/C144_CSharp1/HW2_PrimitiveDataTypesAndVariables/E11_ExchangeTwoVars/E11_ExchangeTwoVars.cs
@@ -0,0 +1,19 @@
+﻿// 11. Declare  two integer variables and assign them with 5 and 10
+//     and after that exchange their values.
+
+class E11_ExchangeTwoVars
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 10;
+        int c = a;
+
+        System.Console.WriteLine("a == {0}; b == {1}", a, b);
+        a = b;
+        b = c;
+        // XOR method a ^= b; b ^= a; a ^= b;
+        
+        System.Console.WriteLine("a == {0}; b == {1}", a, b);
+    }
+}
