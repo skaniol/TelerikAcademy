d0ba0390071e3d5806fb245f0697c7c73f8fb68c
skaniol <skaniol@abv.bg>
2013-11-14 21:20:32 +0200

    Exercises 10 and 13

---
+++ Programming/C144_CSharp1/HW2_PrimitiveDataTypesAndVariables/E10_EmployeeRecord/E10_EmployeeRecord.cs
@@ -0,0 +1,28 @@
+﻿// 10. A marketing firm wants to keep record of its employees.
+//     Each record would have the following characteristics –
+//     first name, family name, age, gender (m or f), ID number,
+//     unique employee number (27560000 to 27569999). 
+//     Declare the variables needed to keep the information for a
+//     single employee using appropriate data types and descriptive names.
+
+using System;
+
+class E10_EmployeeRecord
+{
+    static void Main()
+    {
+        string firstName = "John";
+        string lastName = "Doe";
+        byte age = 30;
+        bool isMale = true;
+        ulong id = 8508083065u;
+        uint employeeNumber = 27563695u;
+
+        Console.WriteLine(
+            "First name: {0}\n Last name: {1}\n       Age: {2}",
+            firstName, lastName, age);
+        Console.WriteLine("    Gender: {0}", isMale ? "m" : "f");
+        Console.WriteLine("      ID \u2116: {0}", id);
+        Console.WriteLine("Employee \u2116: {0}", employeeNumber);
+    }
+}
---
+++ Programming/C144_CSharp1/HW2_PrimitiveDataTypesAndVariables/E13_NullValues/E13_NullValues.cs
@@ -0,0 +1,18 @@
+﻿// 13. Create a program that assigns null values to an integer and to double
+//     variables. Try to print them on the console, try to add some values or
+//     the null literal to them and see the result.
+
+using System;
+
+class E13_NullValues
+{
+    static void Main()
+    {
+        int? intVar = null;
+        double? doubleVar = null;
+        Console.WriteLine("int = {0}, double = {1}", intVar, doubleVar);
+        intVar += 5;
+        doubleVar += 1.5;
+        Console.WriteLine("int = {0}, double = {1}", intVar, doubleVar);
+    }
+}
