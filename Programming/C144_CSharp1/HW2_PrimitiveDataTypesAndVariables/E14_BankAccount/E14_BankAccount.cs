30948e547ef0b9546d91f6534bd27d88f3c112dc
skaniol <skaniol@abv.bg>
2013-11-15 00:06:43 +0200

    E14 -> print variables

--- Programming/C144_CSharp1/HW2_PrimitiveDataTypesAndVariables/E14_BankAccount/E14_BankAccount.cs
+++ Programming/C144_CSharp1/HW2_PrimitiveDataTypesAndVariables/E14_BankAccount/E14_BankAccount.cs
@@ -4,6 +4,8 @@
 //     needed to keep the information for a single bank account using the
 //     appropriate data types and descriptive names.
 
+using System;
+
 class E14_BankAccount
 {
     static void Main()
@@ -15,7 +17,16 @@ class E14_BankAccount
         string bankName = "AXA Banque";
         string iban = "FR7612548029980000000150086";
         string bic = "AXABFRPP";
-        ulong[] creditCard = {
+        ulong[] creditCards = {
               4000080706200002u, 3722316742600000u, 2937816292739723u };
+
+        Console.WriteLine("Account holder: {0} {1} {2}",
+            firstName, middleName, lastName);
+        Console.WriteLine("Balance = {0}", accountBalance);
+        Console.WriteLine("Bank: {0}\tBIC: {1}\tIBAN: {2}",
+            bankName, bic, iban);
+        Console.WriteLine("Credit Cards: {0}\t{1}\t{2}",
+            creditCards[0], creditCards[1], creditCards[2]);
+
     }
 }
