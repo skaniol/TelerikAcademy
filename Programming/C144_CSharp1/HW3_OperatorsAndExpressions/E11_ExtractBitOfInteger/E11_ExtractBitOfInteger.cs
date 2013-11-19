703516f4a0c879f95f886f7df2b9e9e7c7d8267d
skaniol <skaniol@abv.bg>
2013-11-19 16:49:30 +0200

    c144.hw3.e11: Extract Bit Of Integer

---
+++ Programming/C144_CSharp1/HW3_OperatorsAndExpressions/E11_ExtractBitOfInteger/E11_ExtractBitOfInteger.cs
@@ -0,0 +1,28 @@
+﻿// 11. Write an expression that extracts from a given
+//     integer i the value of a given bit number b.
+//     Example: i=5; b=2 -> value=1.
+
+using System;
+
+class E11_ExtractBitOfInteger
+{
+    static void Main()
+    {
+        int i;
+        Console.Write("Enter an integer: ");
+        while (!Int32.TryParse(Console.ReadLine(), out i))
+        {
+            Console.Write("Invalid Integer. Try again: ");
+        }
+
+        byte b;
+        Console.Write("Enter bit position: ");
+        while (!Byte.TryParse(Console.ReadLine(), out b) | b > 31)
+        {
+            Console.Write("Invalid Position. Try again: ");
+        }
+
+        int value = (i >> b) & 1;
+        Console.WriteLine("i = {0}; b = {1} -> value = {2}", i, b, value);
+    }
+}
