0dd6e7e227f3d821ff907d46ed89a6e27337296a
skaniol <skaniol@abv.bg>
2013-11-23 00:55:12 +0200

    slight optimization to c144.hw4.e06
    c144.hw4.e06: Solve Quadratic Equation
    * skip the square root of zero discriminant
    * change type of discriminant (decimal -> double)
        Changed the type 'cause the precision is lost anyways
        (thanks to Math.Sqrt)

--- Programming/C144_CSharp1/HW4_ConsoleInputAndOutput/E06_SolveQuadraticEquation/E06_SolveQuadraticEquation.cs
+++ Programming/C144_CSharp1/HW4_ConsoleInputAndOutput/E06_SolveQuadraticEquation/E06_SolveQuadraticEquation.cs
@@ -12,7 +12,6 @@ class E06_SolveQuadraticEquation
         Console.Write("a = ");
         while (!decimal.TryParse(Console.ReadLine(), out a) || a == 0)
         {
-            if (a == 0) Console.WriteLine("a must not be equal to 0");
             Console.Write("Invalid number! Try again!\na = ");
         }
 
@@ -30,24 +29,24 @@ class E06_SolveQuadraticEquation
             Console.Write("Invalid number! Try again!\nc = ");
         }
 
-        decimal d = b * b - 4 * a * c;
+        double d = (double)(b * b - 4 * a * c);
         if (d > 0)
         {
-            decimal xOne = (-b + (decimal)Math.Sqrt((double)d)) / (2 * a);
-            decimal xTwo = (-b - (decimal)Math.Sqrt((double)d)) / (2 * a);
+            decimal xOne = (-b + (decimal)Math.Sqrt(d)) / (2 * a);
+            decimal xTwo = (-b - (decimal)Math.Sqrt(d)) / (2 * a);
             Console.WriteLine("\nx1 = {0}\nx2 = {1}", xOne, xTwo);
         }
         else if (d == 0)
         {
-            decimal xOne = (-b + (decimal)Math.Sqrt((double)d)) / (2 * a);
+            decimal xOne = -b / (2 * a);
             Console.WriteLine("\nx1 = x2 = {0}", xOne);
         }
         else
         {
             Complex xOne =
-                (-(Complex)b + Complex.Sqrt((double)d)) / (Complex)(2 * a);
+                (-(Complex)b + Complex.Sqrt(d)) / (Complex)(2 * a);
             Complex xTwo =
-                (-(Complex)b - Complex.Sqrt((double)d)) / (Complex)(2 * a);
+                (-(Complex)b - Complex.Sqrt(d)) / (Complex)(2 * a);
             Console.WriteLine("\nx1 = {0} {1}{2}i\nx2 = {3} {4}{5}i",
                 xOne.Real, xOne.Imaginary > 0 ? "+" : "", xOne.Imaginary,
                 xTwo.Real, xTwo.Imaginary > 0 ? "+" : "", xTwo.Imaginary);
