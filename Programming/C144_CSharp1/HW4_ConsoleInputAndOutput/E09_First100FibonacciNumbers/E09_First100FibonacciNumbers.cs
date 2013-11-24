bbd53c230f6e9ef0ed11ef642b7f268fa8e35d42
skaniol <skaniol@abv.bg>
2013-11-24 05:11:16 +0200

    c144.hw4.e09-e10: HomeWork4 Exercises 09-10
    E09 - First 100 Fibonacci Numbers
    E10 - Sum of Sequence

---
+++ Programming/C144_CSharp1/HW4_ConsoleInputAndOutput/E09_First100FibonacciNumbers/E09_First100FibonacciNumbers.cs
@@ -0,0 +1,22 @@
+﻿// 09. Write a program to print the first 100 members of the sequence of 
+//     Fibonacci: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, …
+
+using System;
+
+class E09_First100FibonacciNumbers
+{
+    static void Main()
+    {
+        decimal previous = 0;
+        decimal current = 1;
+
+        Console.Write("0, 1");
+        for (int i = 1; i < 100; i++)
+        {
+            current += previous;
+            Console.Write(", " + current);
+            previous = current - previous;
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Programming/C144_CSharp1/HW4_ConsoleInputAndOutput/E10_SumOfSequence/E10_SumOfSequence.cs
@@ -0,0 +1,24 @@
+﻿// 10. Write a program to calculate the sum (with accuracy of 0.001):
+//     1 + 1/2 - 1/3 + 1/4 - 1/5 + ...
+
+using System;
+
+class E10_SumOfSequence
+{
+    static void Main()
+    {
+        double sum = 1.0;
+        double oldSum;
+        int i = 2;
+        sbyte sign = 1;
+
+        do
+        {
+            oldSum = sum;
+            sum += 1.0 / (i * sign);
+            sign *= -1;
+            i++;
+        } while (Math.Abs(sum - oldSum) > 0.001);
+        Console.WriteLine("{0:F3}", sum);
+    }
+}
